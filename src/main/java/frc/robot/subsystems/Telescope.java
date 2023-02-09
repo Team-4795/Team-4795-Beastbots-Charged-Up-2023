4b2a46245604908e39c03e5b0e5eddebd991fbd4
CRWeddendorf <124752479+CRWeddendorf@users.noreply.github.com>
2023-02-09 16:42:58 -0500

    Update Telescope.java

--- src/main/java/frc/robot/subsystems/Telescope.java
+++ src/main/java/frc/robot/subsystems/Telescope.java
@@ -7,13 +7,11 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.IdleMode;
-import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 public class Telescope extends SubsystemBase {
 
 private final CANSparkMax Bob = new CANSparkMax(2, MotorType.kBrushless);
-
 
     public Telescope() {
 
@@ -29,6 +27,14 @@ private final CANSparkMax Bob = new CANSparkMax(2, MotorType.kBrushless);
 
     public void retract() {
       Bob.set(-.25);
+    }
+
+    public void setSoftLimit(){
+      Bob.enableSoftLimit(CANSparkMax.SoftLimitDirection.kForward, true);
+      Bob.enableSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, true);
+      Bob.setSoftLimit(CANSparkMax.SoftLimitDirection.kForward, 0); //filler values for now
+      Bob.setSoftLimit(CANSparkMax.SoftLimitDirection.kReverse, 0); //filler values for now
+
     }
 
   @Override
