8be08d9bb48e098ac146c0b667cae2caa4584b9b
TheBrainDead <114530240+TheBrainDead@users.noreply.github.com>
2023-02-14 16:39:10 -0500

    Added Rotary Arm with Buttons

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -1,20 +1,23 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class EndEffector {
+public class EndEffector extends SubsystemBase{
     PWMSparkMax lmotor = new PWMSparkMax(0);
     PWMSparkMax rmotor = new PWMSparkMax(1);
     private DoubleSolenoid solenoid = new DoubleSolenoid(null, 0 ,1);
     //Change null and motors later
-    public void CubeIn(double speed){
+    public void CubeIn(){
+        double speed = 0.1;
         lmotor.set(speed);
         rmotor.set(-speed);
     }
     public void ConeIn(){
         solenoid.set(DoubleSolenoid.Value.kForward);
     }
-    public void CubeOut(double speed){
+    public void CubeOut(){
+        double speed = 0.1;
         lmotor.set(-speed);
         rmotor.set(speed);
     }
