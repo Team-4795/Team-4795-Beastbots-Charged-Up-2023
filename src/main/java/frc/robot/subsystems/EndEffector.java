56ccf667933a07c67a5eb1b79cde0776c0b6a8e2
TheBrainDead <114530240+TheBrainDead@users.noreply.github.com>
2023-02-16 16:27:55 -0500

    Update EndEffector.java

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -1,12 +1,13 @@
 package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class EndEffector extends SubsystemBase{
+public class EndEffector extends SubsystemBase  {
     PWMSparkMax lmotor = new PWMSparkMax(0);
     PWMSparkMax rmotor = new PWMSparkMax(1);
-    private DoubleSolenoid solenoid = new DoubleSolenoid(null, 0 ,1);
+    private DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0 ,1);
     //Change null and motors later
     public void CubeIn(){
         double speed = 0.1;
