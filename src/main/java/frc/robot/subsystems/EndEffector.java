50926040253de32ae9bc521895aec76e6ceeaeb2
TheBrainDead <114530240+TheBrainDead@users.noreply.github.com>
2023-02-16 16:38:38 -0500

    Fixed End Effector

--- src/main/java/frc/robot/subsystems/EndEffector.java
+++ src/main/java/frc/robot/subsystems/EndEffector.java
@@ -2,15 +2,14 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class EndEffector extends SubsystemBase  {
+public class EndEffector {
     PWMSparkMax lmotor = new PWMSparkMax(0);
     PWMSparkMax rmotor = new PWMSparkMax(1);
     private DoubleSolenoid solenoid = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, 0 ,1);
     //Change null and motors later
     public void CubeIn(){
-        double speed = 0.1;
+        double speed = 0.25;
         lmotor.set(speed);
         rmotor.set(-speed);
     }
@@ -18,7 +17,7 @@ public class EndEffector extends SubsystemBase  {
         solenoid.set(DoubleSolenoid.Value.kForward);
     }
     public void CubeOut(){
-        double speed = 0.1;
+        double speed = 0.25;
         lmotor.set(-speed);
         rmotor.set(speed);
     }
